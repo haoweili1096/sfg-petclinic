7801d462e727650c03ff280d9677620c286a3f78
haoweili1096 <haoweili007@gmail.com>
2020-11-05 20:49:42 -0800

    Refactoring Map Impl to abstract ID generation. closes #23

--- pet-clinic-data/src/main/java/haowei/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/haowei/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,14 @@
 package haowei.springframework.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import haowei.springframework.sfgpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+//这里有小技巧，因为这是一个抽象类，map.keySet() will be unhappy, 所以这里让ID extends Long,然后把Map里的ID变成Long,就可以了
+//此外，T extends BaseEntity 是为了object.getId() 能够使用BaseEntity的方法
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -17,8 +18,21 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id, object);
+    //原本save(ID id, T object)
+    //现在变得更strong，因为不再需要直到object的id了
+    T save(T object){
+
+        if(object != null){
+            if(object.getId() == null){
+                object.setId(getNextId());
+            }
+
+            map.put(object.getId(), object);
+        }
+        else{
+            throw new RuntimeException("Object cannot be null");
+        }
+
         return object;
     }
 
@@ -29,4 +43,20 @@ public abstract class AbstractMapService<T, ID> {
     void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
+
+    private Long getNextId(){
+
+        Long nextId = null;
+
+        //最初的时候，map中没有对象，那么nextId = null
+        try{
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e){
+            nextId = 1L;
+        }
+
+        return nextId;
+    }
+
+
 }
