cae3a40e00fbe2e925c2a6dba86ec228ebf64b0b
haoweili1096 <haoweili007@gmail.com>
2020-11-05 16:29:28 -0800

    adding bootstrap for data. closes #14.

--- pet-clinic-data/src/main/java/haowei/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/haowei/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package haowei.springframework.sfgpetclinic.services.map;
 
 import haowei.springframework.sfgpetclinic.model.Vet;
-import haowei.springframework.sfgpetclinic.services.CrudService;
+import haowei.springframework.sfgpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
     public Set<Vet> findAll() {
