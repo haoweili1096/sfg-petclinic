c99ac7c310c71c81892e590384cb4c5b60f09455
haoweili1096 <haoweili007@gmail.com>
2020-11-06 18:58:18 -0800

    added specialities map impl. closes #31.

---
+++ pet-clinic-data/src/main/java/haowei/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package haowei.springframework.sfgpetclinic.services;
+
+import haowei.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long>{
+
+}
