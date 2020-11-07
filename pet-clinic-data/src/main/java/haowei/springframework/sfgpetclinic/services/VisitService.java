338cc921297ce11691238f6000c9933c46c0c586
haoweili1096 <haoweili007@gmail.com>
2020-11-07 13:08:06 -0800

    Implement map Impl for Visit Service. closes #46.

---
+++ pet-clinic-data/src/main/java/haowei/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package haowei.springframework.sfgpetclinic.services;
+
+import haowei.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
