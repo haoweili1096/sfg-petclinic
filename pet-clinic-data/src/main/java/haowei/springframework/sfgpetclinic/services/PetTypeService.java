bab3778ff9c3e310b181220a2f1bf924bde68e8f
haoweili1096 <haoweili007@gmail.com>
2020-11-06 18:06:55 -0800

    added PetTypeService. closes #29.

---
+++ pet-clinic-data/src/main/java/haowei/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package haowei.springframework.sfgpetclinic.services;
+
+import haowei.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}
